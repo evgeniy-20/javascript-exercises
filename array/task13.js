f13805c97471bd271cae852ba22e868cda69d733
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-05-15 17:02:14 +0300

    task14

---
+++ array/task13.js
@@ -0,0 +1,4 @@
+var furryAnimals = ["Alpaca" , "Ring-tailed Lemer" , "Yeti"];
+var scalyAnimals = ["Boa Constrictor" , "Godzilla"];
+var furryAndScalyAnimals = furryAnimals.concat(scalyAnimals);
+console.log(furryAndScalyAnimals);
