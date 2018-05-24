a24a7a923bbcd3dd1ef1cafd974e83278f9d9a05
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-05-24 22:10:21 +0300

    task17

---
+++ object/task17.js
@@ -0,0 +1,8 @@
+var dinosaurs = [
+    { name: "Tyrannosaurus Rex" , period: "Late Cretaceous" } ,
+    { name: "Stegosaurus" , period: "Late Jurassic" } ,
+    { name: "Plateosaurus" , period: "Triassic"}
+];
+console.log(dinosaurs[0]);
+console.log(dinosaurs[0]["name"]);
+console.log(dinosaurs[1].period);
