80214985baa55519353cb70f2de6542ce078f05a
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-05-24 21:20:34 +0300

    task15

---
+++ object/task15.js
@@ -0,0 +1,4 @@
+var dog = { name: "Pancake", age: 6, color: "white", bark: "Yip yap yip"};
+var cat  = { name: "Harmony", age: 8, color: "tortoiseshell"};
+console.log(Object.keys(dog));
+console.log(Object.keys(cat));
