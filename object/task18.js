b3efd198ebb4a69a01f5c9a830e293fda426fd25
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-05-25 21:37:31 +0300

    task18

---
+++ object/task18.js
@@ -0,0 +1,5 @@
+var stas = { name: "Stas", age: 26, luckyNumbers: [6, 8, 16,32] };
+var yra = { name: "Yra", age: 24, luckyNumbers: [5, 9, 25,56] };
+var alina = { name: "Alina", age: 18, luckyNumbers: [6, 8, 32] };
+var friends = [stas , yra , alina];
+console.log(friends[1]);
