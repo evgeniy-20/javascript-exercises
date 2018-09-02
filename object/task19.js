057365eaa357207e664dc6a45c2d96c82a3a78bc
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-09-02 08:57:28 +0300

    task19

---
+++ object/task19.js
@@ -0,0 +1,7 @@
+var owedMoney = {};
+owedMoney["Stas"] = 10;
+owedMoney["Yura"] = 20;
+console.log(owedMoney["Stas"]);
+owedMoney["Stas"] += 50;
+console.log(owedMoney["Stas"]);
+console.log(owedMoney);
