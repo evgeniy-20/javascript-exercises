c4098ae940087e4e9f64996593af502fd4656c9f
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-09-22 20:28:37 +0300

    task2

--- example/task2.js
+++ example/task2.js
@@ -1,6 +1,7 @@
+// Малюємо стільки котів, скільки захочемо!
 var drawCats = function (howManyTimes) {
     for (var i = 0; i < howManyTimes; i++) {
         console.log(i + " =^.^=");
     }
 };
-console.log(drawCats(22));
+console.log(drawCats(22)); // Замість 22 можете вказати будь-яке число, яке вам заманеться.
