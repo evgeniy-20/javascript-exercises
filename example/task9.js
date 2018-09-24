e9d0ae87a928c411a43ed722603d379542f6f8da
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-09-24 20:32:46 +0300

    task9

--- example/task9.js
+++ example/task9.js
@@ -2,3 +2,14 @@ var secondsInAMinute = 60;
 var minutesInAnHour = 60;
 secondsInAnHour = secondsInAMinute * minutesInAnHour;
 console.log(secondsInAnHour);
+
+var hoursInADay = 24;
+var secondsInADay = secondsInAnHour * hoursInADay;
+console.log(secondsInADay);
+
+var daysInAYear = 365;
+var secondsInAYear = secondsInADay * daysInAYear;
+console.log(secondsInAYear);
+
+var age = 21;
+console.log(age * secondsInAYear);
