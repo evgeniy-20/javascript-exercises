474a41e02b2d356dd8b84770f447a78e1235220e
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-10-12 21:07:25 +0300

    task14

--- object/task14.js
+++ object/task14.js
@@ -3,3 +3,18 @@ var cat = {
     "name": "Harmony",
     "color": "Tortoiseshell"
 };
+console.log(cat);
+var cat = {
+    legs: 3,
+    name: "Harmony",
+    color: "Tortoiseshell"
+};
+console.log(cat);
+var cat = {
+    legs: 3,
+    "full name": "Harmony Philomena Snuggly-Pants Morgan",
+    color: "Tortoiseshell"
+};
+console.log(cat);
+var cat = { legs: 3, name: "Harmony", color: "Tortoiseshell" };
+console.log(cat);
