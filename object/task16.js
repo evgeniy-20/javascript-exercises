3b54636bb224a38f7b94129c8ea875139a715341
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-10-13 20:58:48 +0300

    task16

--- object/task16.js
+++ object/task16.js
@@ -3,3 +3,14 @@ cat["legs"] = 3;
 cat["name"] = "Harmony";
 cat["color"] = "Tortoiseshell";
 console.log(cat);
+var cat = {};
+cat.legs = 3;
+cat.name = "Harmony";
+cat.color = "Tortoiseshell";
+console.log(cat);
+var dog = {
+    name: "Pancake",
+    legs: 4,
+    isAwesome: true 
+};
+console.log(dog.isBrown);
