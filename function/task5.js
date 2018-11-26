7a3fefd64d6a7a64385ee0c0dbe50b532b14703c
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2018-11-26 20:30:02 +0200

    task5

--- function/task5.js
+++ function/task5.js
@@ -2,4 +2,7 @@ console.log(15 + Math.floor(2.456));
 var double = function (number) {
     return number * 2;
 };
-double(3);
+double(double(3));
+    double(3 * 2);
+        double(6);
+            double(6 * 2);
