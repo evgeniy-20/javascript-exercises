eab3a52e25c574f1c20739434305777f67f915ac
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2019-04-24 19:42:11 +0300

    essence of the object and the array

---
+++ object/essence of the object and the array.js
@@ -0,0 +1,6 @@
+var myCrazyObject = {
+    "name": "A radiculous object",
+    "somearray": [7, 9, {purpose: "confusion", number: 123}, 3.3],
+    "randomanimal": "Banana Shark"
+};
+console.log(myCrazyObject["somearray"][2]["number"]);
--- object/task21.js
+++ object/task21.js
@@ -1,7 +1,1 @@
 
-var myCrazyObject = {
-    "name": "A radiculous object",
-    "somearray": [7, 9, {purpose: "confusion", number: 123}, 3.3],
-    "randomanimal": "Banana Shark"
-};
-console.log(myCrazyObject["somearray"][2]["number"]);
