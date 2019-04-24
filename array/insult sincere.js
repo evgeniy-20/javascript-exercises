4a472655a1e772277b2fb256b7d801b8d71985dc
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2019-04-24 19:33:28 +0300

    insult sincere

--- array/insult sincere.js
+++ array/insult sincere.js
@@ -1,9 +1,9 @@
-var randomBodyParts = ["глаз", "нос", "череп"];
+var randomBodyParts = ["глаз", "нос", "череп", "дупа"];
 var randomAnimalBodyParts = ["хвост", "пузо", "рот"];
-var randomAdjectives = ["вонючий", "унылый", "дурацкий"];
+var randomAdjectives = ["вонючий", "унылая", "дурацкий"];
 var randomAnimals = ["жабы", "выдры", "коровы", "мартышки", "крысы"];
 
-var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 4)];
 var randomAnimalBodyPart = randomAnimalBodyParts[Math.floor(Math.random() * 3)];
 var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
 var randomAnimal = randomAnimals[Math.floor(Math.random() * 5)];
