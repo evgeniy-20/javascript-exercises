c6aaba8daef308b7a0a1918b68b0c4d3146536be
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2019-04-24 19:40:13 +0300

    points counter

--- object/task21.js
+++ object/task21.js
@@ -1,36 +1,4 @@
-var scores = {};
-scores["father"] = 0;
-scores["mather"] = 0;
-scores["Stas"] = 0;
-scores["Yura"] = 0;
-scores["Evgen"] = 0;
-scores["Alina"] = 0;
-scores["Katya"] =  0;
-scores["Sophia"] = 0;
-console.log(scores["father"]);
-console.log(scores["mather"]);
-console.log(scores["Stas"]);
-console.log(scores["Yura"]);
-console.log(scores["Evgen"]);
-console.log(scores["Alina"]);
-console.log(scores["Katya"]);
-console.log(scores["Sophia"]);
-scores["father"] += 20;
-console.log(scores["father"]);
-scores["mather"] += 19;
-console.log(scores["mather"]);
-scores["Stas"] += 15;
-console.log(scores["Stas"]);
-scores["Yura"] += 12;
-console.log(scores["Yura"]);
-scores["Evgen"] += 10;
-console.log(scores["Evgen"]);
-scores["Alina"] += 8;
-console.log(scores["Alina"]);
-scores["Katya"] += 6;
-console.log(scores["Katya"]);
-scores["Sophia"] += 4;
-console.log(scores["Sophia"]);
+
 var myCrazyObject = {
     "name": "A radiculous object",
     "somearray": [7, 9, {purpose: "confusion", number: 123}, 3.3],
