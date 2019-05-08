35ba1c79d90cf0528dde50aa464cd514e1cd72a6
Evgeniy Prokopiv <evgeniy.prokopiv@gmail.com>
2019-05-08 05:44:16 +0300

    randomNumber

--- function/randomNumber.js
+++ function/randomNumber.js
@@ -1,5 +1,5 @@
+var randomNumbers = [0,1,2,3,4,5,6,7,8,9];
 var getRandomNumber = function (number) {
     return number[Math.floor(Math.random() * number.length)];
 };
-var randomNumbers = [0,1,2,3,4,5,6,7,8,9];
 console.log(getRandomNumber(randomNumbers));
